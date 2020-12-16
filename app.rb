1d8364aace9cdb6f4fc10264a43f07f80b710fe4
okorkov <alex.okorkov@gmail.com>
2020-12-16 15:26:50 -0800

    commit tree 64cdfb3a17f2ea28629cf8ee7057757e9c7452ca

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, "secret"
+
+  get '/' do
+    @session = session
+    erb :index
+  end
+
+  post '/checkout' do 
+    @session = session
+    @params = params
+    # @params[:session] = @session
+    @session[:item] = @params[:item]
+    "#{@params}"
+  end
+
 end
